c57c85ae145cbf33f2d95a6a8734c38c766fda1d
szrtGreg <szrt@poczta.onet.pl>
2017-07-26 13:36:56 +0200

    Add Movie Init

---
+++ VidlyF.Web/Controllers/MoviesController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using VidlyF.Web.Models;
+
+namespace VidlyF.Web.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+              new Movie { Id = 1, Name = "Movie1" },
+              new Movie { Id = 2, Name = "Movie2" }
+            };
+        }
+    }
+}
---
+++ VidlyF.Web/Models/Movie.cs
@@ -0,0 +1,8 @@
+﻿namespace VidlyF.Web.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
