687b399abc0a79cab207467b66abfeaa067f0043
szrtGreg <szrt@poczta.onet.pl>
2017-07-26 14:49:53 +0200

    adding adnotation

---
+++ VidlyF.Web/Migrations/201707261210349_AddingAdnotation.cs
@@ -0,0 +1,20 @@
+namespace VidlyF.Web.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddingAdnotation : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String(nullable: false, maxLength: 255));
+            AlterColumn("dbo.Movies", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Movies", "Name", c => c.String());
+            AlterColumn("dbo.Customers", "Name", c => c.String());
+        }
+    }
+}
--- VidlyF.Web/Models/Customer.cs
+++ VidlyF.Web/Models/Customer.cs
@@ -1,13 +1,18 @@
-﻿namespace VidlyF.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VidlyF.Web.Models
 {
     public class Customer
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
 
+        [Required]
         public MembershipType MembershipType { get; set; }
 
         public byte MembershipTypeId { get; set; }
