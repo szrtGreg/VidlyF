f487a61fae292fa9ad0d62ac5ee7d628a581e1a3
szrtGreg <szrt@poczta.onet.pl>
2017-07-26 13:56:53 +0200

    Adding MembershipType and Relation

--- VidlyF.Web/Models/IdentityModels.cs
+++ VidlyF.Web/Models/IdentityModels.cs
@@ -1,8 +1,8 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace VidlyF.Web.Models
 {
@@ -20,6 +20,9 @@ namespace VidlyF.Web.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<MembershipType> MembershipTypes { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
