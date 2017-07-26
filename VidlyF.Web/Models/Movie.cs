a87f94432c62a3004d3512c1342459cec74ee645
szrtGreg <szrt@poczta.onet.pl>
2017-07-26 13:59:55 +0200

    Adding Genres and Relations

--- VidlyF.Web/Models/Movie.cs
+++ VidlyF.Web/Models/Movie.cs
@@ -1,8 +1,23 @@
-﻿namespace VidlyF.Web.Models
+﻿using System;
+
+namespace VidlyF.Web.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public byte NumberInStock { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public byte GenreId { get; set; }
+
+        public byte NumberAvailable { get; set; }
     }
 }
