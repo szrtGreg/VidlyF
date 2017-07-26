d1b838b2a7cc301a518c45286e6b1840ac2e020e
szrtGreg <szrt@poczta.onet.pl>
2017-07-26 13:28:50 +0200

    Add Customer Init

---
+++ VidlyF.Web/Controllers/CustomersController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using VidlyF.Web.Models;
+
+namespace VidlyF.Web.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+              new Customer { Id = 1, Name = "Client1" },
+              new Customer { Id = 2, Name = "Client2" }
+            };
+        }
+    }
+}
